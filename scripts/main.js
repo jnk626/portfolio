8c0cd16a59bdc9e2d67f3e2ddb7bb4d9f77a8864
jnk626 <jnk626@pm.me>
2022-12-16 18:32:02 +0100

    0.61: completed and sent

---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const menu = document.getElementById('sidebar');
+const mainContent = document.getElementById('main-content');
+// Selects both the links inside the menu and the toggle button
+const navLinks = document.querySelectorAll('li.nav-links, span.nav-links');
+const logoText = document.getElementById('logo-text');
+
+// To prevent the sidebar to cover the page on loading
+// on mobile devices
+if (document.documentElement.clientWidth < 401) {
+    menu.classList.add('closed');
+    mainContent.classList.add('closed');
+}
+
+// By clicking a menu link the sidebar will close while moving
+// to the section selected
+navLinks.forEach((link) => {
+    link.onclick = function() {
+        menu.classList.toggle('closed');
+        mainContent.classList.toggle('closed');
+    }
+})
+
+// Classic user behaviour, logo = top of the site
+logoText.onclick = () => window.scrollTo(0,0);
+
+
+// EmailJS
+window.onload = function() {
+    const form = document.getElementById('contact-form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+                form.append("Il tuo messaggio è stato inviato!");
+            }, function(error) {
+                console.log('FAILED...', error);
+                form.append("Errore! Ricarica la pagina e riprova.");
+            });
+    });
+}
+
